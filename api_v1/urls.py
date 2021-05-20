dad61671a0511914e1e1959a4b2f9776e0236680
Kurbanaliev <kurbanalieverlan@gmail.com>
2021-05-20 17:22:48 +0600

    ревлизовал первое задание

--- api_v1/urls.py
+++ api_v1/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from api_v1.views import my_first_api_view
+from api_v1.views import api_add, get_token_view, subtract_api, multiply_api, divide_api
+
 app_name = 'api_v1'
 
 urlpatterns = [
-    path('add/', my_first_api_view, name='add_api')
+    path('add/', api_add, name='add_api'),
+    path('subtract/', subtract_api, name='subtract_api'),
+    path('multiply/', multiply_api, name='multiply_api'),
+    path('divide/', divide_api, name='divide_api'),
+    path('get_token/', get_token_view, name='get_token'),
 ]
--- api_v1/views.py
+++ api_v1/views.py
@@ -1,14 +1,80 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotAllowed
 from django.shortcuts import render
 import json
 # Create your views here.
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 
-def my_first_api_view(request):
-    response_data = {
-     "A": 1234,
-     "B": 4567
-}
-    response = HttpResponse(json.dumps(response_data))
+@ensure_csrf_cookie
+def get_token_view(request, *args, **kwargs):
+    if request.method == 'GET':
+        return HttpResponse()
+    return HttpResponseNotAllowed('Only GET request are allowed')
+
+
+def api_add(request):
+    if request.body:
+        try:
+            response_data = json.loads(request.body)
+            add = sum(response_data.values())
+            response_answer = {
+                "answer": add
+            }
+            response = HttpResponse(json.dumps(response_answer))
+            response['Content-Type'] = 'application/json'
+            return response
+        except:
+            return for_exept()
+
+def subtract_api(request):
+    if request.body:
+        try:
+            response_data = json.loads(request.body)
+            subtract = response_data['A'] - response_data['B']
+            response_answer = {
+                "answer":  subtract
+            }
+            response = HttpResponse(json.dumps(response_answer))
+            response['Content-Type'] = 'application/json'
+            return response
+        except:
+            return for_exept()
+
+
+def multiply_api(request):
+    if request.body:
+        try:
+            response_data = json.loads(request.body)
+            multiply = response_data['A'] * response_data['B']
+            response_answer = {
+                "answer":  multiply
+            }
+            response = HttpResponse(json.dumps(response_answer))
+            response['Content-Type'] = 'application/json'
+            return response
+        except:
+            return for_exept()
+
+def divide_api(request):
+    if request.body:
+        try:
+            response_data = json.loads(request.body)
+            divide = response_data['A'] / response_data['B']
+            response_answer = {
+                "answer":  divide
+            }
+            response = HttpResponse(json.dumps(response_answer))
+            response['Content-Type'] = 'application/json'
+            return response
+        except:
+            return for_exept()
+
+def for_exept():
+    data = 'ERROR IN REQUEST XA XA XA'
+    response_answer = {
+        "answer": data
+    }
+    response = HttpResponse(json.dumps(response_answer))
     response['Content-Type'] = 'application/json'
+    response.status_code = 400
     return response
